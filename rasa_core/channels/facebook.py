6e7f0527f2c97a53845a91bfaf83e42b9e6c2bd2
Leonardo Custodio <leonardo@custodio.me>
2019-02-19 10:28:08 -0300

    Added analytics

--- rasa_core/channels/facebook.py
+++ rasa_core/channels/facebook.py
@@ -7,10 +7,12 @@ from fbmessenger import (
     BaseMessenger, MessengerClient, attachments)
 from fbmessenger.elements import Text as FBText
 from flask import Blueprint, request, jsonify
+from dashbot import generic
 
 from rasa_core.channels.channel import UserMessage, OutputChannel, InputChannel
 
 logger = logging.getLogger(__name__)
+dba = generic.generic('pEMhy004oemaEIoBYVXvJIlvSxDDM4F75KdcU2R7')
 
 
 class Messenger(BaseMessenger):
@@ -66,6 +68,7 @@ class Messenger(BaseMessenger):
     def message(self, message: Dict[Text, Any]) -> None:
         """Handle an incoming event from the fb webhook."""
         print("Message: {}".format(message))
+        dba.logIncoming(message)
 
         if self._is_user_message(message):
             text = message['message']['text']
